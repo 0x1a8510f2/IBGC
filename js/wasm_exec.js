c2e43d6dc35d4e7ba92bad87128fe85ae16ac3e8
TR-SLimey <tr_slimey@protonmail.com>
2020-09-04 19:09:49 +0100

    Made indentation more uniform in files

--- js/wasm_exec.js
+++ js/wasm_exec.js
@@ -40,194 +40,194 @@
 		return err;
 	};
 
-    // Added
-    const filesystem = {};
+	// Added
+	const filesystem = {};
 
-    let workingDirectory = '/';
+	let workingDirectory = '/';
 
-    let absPath = (path) => {
-        if (path[0] == '/') {
-            return path;
-        }
-        return workingDirectory + path.replace(/^\.\/?/, '');
-    };
+	let absPath = (path) => {
+		if (path[0] == '/') {
+			return path;
+		}
+		return workingDirectory + path.replace(/^\.\/?/, '');
+	};
 
-    global.readFromGoFilesystem = (path) => filesystem[absPath(path)];
-    global.writeToGoFilesystem = (path, content) => {
-        if (typeof content === 'string') {
-            filesystem[absPath(path)] = encoder.encode(content);
-        } else {
-            filesystem[absPath(path)] = content;
-        }
-    };
-    global.goStdout = (buf) => {};
-    global.goStderr = (buf) => {};
+	global.readFromGoFilesystem = (path) => filesystem[absPath(path)];
+	global.writeToGoFilesystem = (path, content) => {
+		if (typeof content === 'string') {
+			filesystem[absPath(path)] = encoder.encode(content);
+		} else {
+			filesystem[absPath(path)] = content;
+		}
+	};
+	global.goStdout = (buf) => {};
+	global.goStderr = (buf) => {};
 
-    const openFiles = new Map();
-    let nextFd = 1000;
+	const openFiles = new Map();
+	let nextFd = 1000;
 
-    let stat = (path, callback) => {
-        let mode = 0;
-        if (path === '/') {
-            mode |= 0x80000000;
-        } else if (filesystem[path] === undefined) {
-            const err = new Error('no such file');
-            err.code = 'ENOENT';
-            callback(err);
-            return;
-        }
-        callback(null, {
-            mode,
-            dev: 0,
-            ino: 0,
-            nlink: 0,
-            uid: 0,
-            gid: 0,
-            rdev: 0,
-            size: 0,
-            blksize: 0,
-            blocks: 0,
-            atimeMs: 0,
-            mtimeMs: 0,
-            ctimeMs: 0,
-            isDirectory: () => !!(mode & 0x80000000),
-        });
-    };
+	let stat = (path, callback) => {
+		let mode = 0;
+		if (path === '/') {
+			mode |= 0x80000000;
+		} else if (filesystem[path] === undefined) {
+			const err = new Error('no such file');
+			err.code = 'ENOENT';
+			callback(err);
+			return;
+		}
+		callback(null, {
+			mode,
+			dev: 0,
+			ino: 0,
+			nlink: 0,
+			uid: 0,
+			gid: 0,
+			rdev: 0,
+			size: 0,
+			blksize: 0,
+			blocks: 0,
+			atimeMs: 0,
+			mtimeMs: 0,
+			ctimeMs: 0,
+			isDirectory: () => !!(mode & 0x80000000),
+		});
+	};
 
-    const constants = {
-        O_WRONLY: 1 << 0,
-        O_RDWR: 1 << 1,
-        O_CREAT: 1 << 2,
-        O_TRUNC: 1 << 3,
-        O_APPEND: 1 << 4,
-        O_EXCL: 1 << 5,
-    };
+	const constants = {
+		O_WRONLY: 1 << 0,
+		O_RDWR: 1 << 1,
+		O_CREAT: 1 << 2,
+		O_TRUNC: 1 << 3,
+		O_APPEND: 1 << 4,
+		O_EXCL: 1 << 5,
+	};
 
 	if (!global.fs) {
-        let outputBuf = "";
+		let outputBuf = "";
 		global.fs = {
 
-            constants,
+			constants,
 
 			writeSync(fd, buf) { console.log("writeSync");
-                if (fd === 1) {
-                    global.goStdout(buf);
-                } else if (fd === 2) {
-                    global.goStderr(buf);
-                } else {
-                    const file = openFiles[fd];
-                    const source = filesystem[file.path];
-                    let destLength = source.length + buf.length;
-                    if (file.offset < source.length) {
-                        destLength = file.offset + buf.length;
-                        if (destLength < source.length) {
-                            destLength = source.length;
-                        }
-                    }
-                    const dest = new Uint8Array(destLength);
-                    for (let i = 0; i < source.length; ++i) {
-                        dest[i] = source[i];
-                    }
-                    for (let i = 0; i < buf.length; ++i) {
-                        dest[file.offset + i] = buf[i];
-                    }
-                    openFiles[fd].offset += buf.length;
-                    filesystem[file.path] = dest;
-                }
+				if (fd === 1) {
+					global.goStdout(buf);
+				} else if (fd === 2) {
+					global.goStderr(buf);
+				} else {
+					const file = openFiles[fd];
+					const source = filesystem[file.path];
+					let destLength = source.length + buf.length;
+					if (file.offset < source.length) {
+						destLength = file.offset + buf.length;
+						if (destLength < source.length) {
+							destLength = source.length;
+						}
+					}
+					const dest = new Uint8Array(destLength);
+					for (let i = 0; i < source.length; ++i) {
+						dest[i] = source[i];
+					}
+					for (let i = 0; i < buf.length; ++i) {
+						dest[file.offset + i] = buf[i];
+					}
+					openFiles[fd].offset += buf.length;
+					filesystem[file.path] = dest;
+				}
 			},
 			write(fd, buf, offset, length, position, callback) { console.log("write");
-                if (offset !== 0 || length !== buf.length) {
-                    throw new Error('write not fully implemented: ' + offset + ', ' + length + '/' + buf.length);
-                }
-                if (position !== null) {
-                    openFiles[fd].offset = position;
-                }
-                this.writeSync(fd, buf);
-                callback(null, length);
+				if (offset !== 0 || length !== buf.length) {
+					throw new Error('write not fully implemented: ' + offset + ', ' + length + '/' + buf.length);
+				}
+				if (position !== null) {
+					openFiles[fd].offset = position;
+				}
+				this.writeSync(fd, buf);
+				callback(null, length);
 			},
 			chmod(path, mode, callback) { console.log("chmod"); callback(enosys()); },
 			chown(path, uid, gid, callback) { console.log("chown"); callback(enosys()); },
-            close(fd, callback) {
-                console.log('close(' + fd + ')');
-                openFiles.delete(fd);
-                callback(null);
-            },
+			close(fd, callback) {
+				console.log('close(' + fd + ')');
+				openFiles.delete(fd);
+				callback(null);
+			},
 			fchmod(fd, mode, callback) {
-                console.log('fchmod(' + fd + ', ' + mode + ')');
-                callback(null);
-            },
+				console.log('fchmod(' + fd + ', ' + mode + ')');
+				callback(null);
+			},
 			fchown(fd, uid, gid, callback) { console.log("fchown"); callback(enosys()); },
 			fstat(fd, callback) {
-                console.log('fstat(' + fd + ')');
-                stat(openFiles[fd].path, callback);
-            },
+				console.log('fstat(' + fd + ')');
+				stat(openFiles[fd].path, callback);
+			},
 			fsync(fd, callback) {
-                callback(null);
-            },
+				callback(null);
+			},
 			ftruncate(fd, length, callback) { console.log("ftruncate"); callback(enosys()); },
 			lchown(path, uid, gid, callback) { console.log("lchown"); callback(enosys()); },
 			link(path, link, callback) { console.log("link"); callback(enosys()); },
 			lstat(path, callback) {
-                console.log('lstat(' + path + ')');
-                stat(absPath(path), callback);
-            },
+				console.log('lstat(' + path + ')');
+				stat(absPath(path), callback);
+			},
 			mkdir(path, perm, callback) { console.log("mkdir"); callback(null); },
-            open(path, flags, mode, callback) {
-                console.log('open(' + path + ', ' + mode + ')');
-                path = absPath(path);
-                if (!filesystem[path]) {
-                    if (flags & constants.O_CREAT) {
-                        filesystem[path] = new Uint8Array(0);
-                    } else {
-                        const err = new Error('no such file');
-                        err.code = 'ENOENT';
-                        callback(err);
-                        return;
-                    }
-                }
-                if (flags & constants.O_TRUNC) {
-                    filesystem[path] = new Uint8Array(0);
-                }
-                const fd = nextFd++;
-                openFiles[fd] = {
-                    offset: 0,
-                    path,
-                };
-                callback(null, fd);
-            },
-            read(fd, buffer, offset, length, position, callback) {
-                if (offset !== 0) {
-                    throw new Error('read not fully implemented: ' + offset);
-                }
-                if (position !== null) {
-                    openFiles[fd].offset = position;
-                }
-                const file = openFiles[fd];
-                const source = filesystem[file.path];
-                let n = length;
-                if (file.offset + length > source.length) {
-                    n = source.length - file.offset;
-                }
-                for (let i = 0; i < n; ++i) {
-                    buffer[i] = source[file.offset + i];
-                }
-                openFiles[fd].offset += n;
-                callback(null, n);
-            },
+			open(path, flags, mode, callback) {
+				console.log('open(' + path + ', ' + mode + ')');
+				path = absPath(path);
+				if (!filesystem[path]) {
+					if (flags & constants.O_CREAT) {
+						filesystem[path] = new Uint8Array(0);
+					} else {
+						const err = new Error('no such file');
+						err.code = 'ENOENT';
+						callback(err);
+						return;
+					}
+				}
+				if (flags & constants.O_TRUNC) {
+					filesystem[path] = new Uint8Array(0);
+				}
+				const fd = nextFd++;
+				openFiles[fd] = {
+					offset: 0,
+					path,
+				};
+				callback(null, fd);
+			},
+			read(fd, buffer, offset, length, position, callback) {
+				if (offset !== 0) {
+					throw new Error('read not fully implemented: ' + offset);
+				}
+				if (position !== null) {
+					openFiles[fd].offset = position;
+				}
+				const file = openFiles[fd];
+				const source = filesystem[file.path];
+				let n = length;
+				if (file.offset + length > source.length) {
+					n = source.length - file.offset;
+				}
+				for (let i = 0; i < n; ++i) {
+					buffer[i] = source[file.offset + i];
+				}
+				openFiles[fd].offset += n;
+				callback(null, n);
+			},
 			readdir(path, callback) { console.log("readdir"); callback(enosys()); },
 			readlink(path, callback) { console.log("readlink"); callback(enosys()); },
 			rename(from, to, callback) { console.log("rename"); callback(enosys()); },
 			rmdir(path, callback) { cconsole.log("rmdir"); allback(enosys()); },
 			stat(path, callback) {
-                console.log('stat(' + path + ')');
-                stat(absPath(path), callback);
-            },
+				console.log('stat(' + path + ')');
+				stat(absPath(path), callback);
+			},
 			symlink(path, link, callback) { console.log("symlink"); callback(enosys()); },
 			truncate(path, length, callback) { console.log("truncate"); callback(enosys()); },
 			unlink(path, callback) {
-                console.log('unlink(' + path + ')');
-                callback(null);
-            },
+				console.log('unlink(' + path + ')');
+				callback(null);
+			},
 			utimes(path, atime, mtime, callback) { console.log("utimes"); callback(enosys()); },
 		};
 	}
@@ -243,9 +243,9 @@
 			ppid: -1,
 			umask() { throw enosys(); },
 			cwd() {
-                console.log('cwd()');
-                return workingDirectory;
-            },
+				console.log('cwd()');
+				return workingDirectory;
+			},
 			chdir() { throw enosys(); },
 		}
 	}
